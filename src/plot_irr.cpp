ef9884256d566ba9bc8defcc29662c3239a6a24e
nevrome <clemens@nevrome.de>
2017-07-01 21:15:25 +0200

    added wasd movement controls

--- src/plot_irr.cpp
+++ src/plot_irr.cpp
@@ -189,7 +189,32 @@ bool plot_irr(
   //bill->setTextColor(video::SColor(255,0,0,0));
   
   // add a first person shooter style user controlled camera
-  ICameraSceneNode *camera = scenemgr->addCameraSceneNodeFPS();
+  // Key map added to allow multiple keys for actions such as
+  // movement. ie Arrow keys & W,S,A,D.
+  SKeyMap keyMap[8];
+  keyMap[0].Action = EKA_MOVE_FORWARD;
+  keyMap[0].KeyCode = KEY_UP;
+  keyMap[1].Action = EKA_MOVE_FORWARD;
+  keyMap[1].KeyCode = KEY_KEY_W;
+  
+  keyMap[2].Action = EKA_MOVE_BACKWARD;
+  keyMap[2].KeyCode = KEY_DOWN;
+  keyMap[3].Action = EKA_MOVE_BACKWARD;
+  keyMap[3].KeyCode = KEY_KEY_S;
+  
+  keyMap[4].Action = EKA_STRAFE_LEFT;
+  keyMap[4].KeyCode = KEY_LEFT;
+  keyMap[5].Action = EKA_STRAFE_LEFT;
+  keyMap[5].KeyCode = KEY_KEY_A;
+  
+  keyMap[6].Action = EKA_STRAFE_RIGHT;
+  keyMap[6].KeyCode = KEY_RIGHT;
+  keyMap[7].Action = EKA_STRAFE_RIGHT;
+  keyMap[7].KeyCode = KEY_KEY_D;
+  
+  ICameraSceneNode *camera = scenemgr->addCameraSceneNodeFPS(
+    0, 100.0f, 0.5f, -1, keyMap, 8
+  );
 
   // global light
   //scenemgr->setAmbientLight(video::SColorf(0.3,0.3,0.3,1));
